4d83533ddc36fac76262ea4adb9147cae23d83a8
Abdella Ali <abdella@rangle.io>
2021-06-10 12:02:08 -0400

    test update

--- epub-build.script.js
+++ epub-build.script.js
@@ -6,14 +6,14 @@ fs.readdir('./part-i-the-big-picture', (err, filenames) => {
       return console.log(err);
     }
 
-  filenames.map((fileName) => {
-    return {
-      path: `./part-i-the-big-picture/${fileName}`,
-      updatedContent: fs.readFileSync(`./part-i-the-big-picture/${fileName}`, {encoding:'utf8', flag:'r'}).replaceAll(/..\/(.*\.png)/ig, '../../../../../$1')
-    }
-  }).forEach(fileData => {
-    fs.writeFileSync(fileData.path, fileData.updatedContent, 'utf8');
-  });
+  // filenames.map((fileName) => {
+  //   return {
+  //     path: `./part-i-the-big-picture/${fileName}`,
+  //     updatedContent: fs.readFileSync(`./part-i-the-big-picture/${fileName}`, {encoding:'utf8', flag:'r'}).replaceAll(/..\/(.*\.png)/ig, '../../../../../$1')
+  //   }
+  // }).forEach(fileData => {
+  //   fs.writeFileSync(fileData.path, fileData.updatedContent, 'utf8');
+  // });
   const epubScript = malta.get().check(["the_better_way.json", ".gitbook", "-plugins=malta-epub"]).start();
 
   epubScript.then(() => {
