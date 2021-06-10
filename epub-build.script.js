8e1603d5440d0750f100c53a3ec67c794d11cf89
Abdella Ali <abdella@rangle.io>
2021-06-10 11:57:10 -0400

    updated build script

--- epub-build.script.js
+++ epub-build.script.js
@@ -1,9 +1,24 @@
 const malta = require('malta');
+const fs = require('fs');
 
-// todo: have the configuration json actually get generated dynamically
+fs.readdir('./part-i-the-big-picture', (err, filenames) => {
+    if (err) {
+      return console.log(err);
+    }
 
-const epubScript = malta.get().check(["the_better_way.json", ".gitbook", "-plugins=malta-epub"]).start();
+  filenames.map((fileName) => {
+    return {
+      path: `./part-i-the-big-picture/${fileName}`,
+      updatedContent: fs.readFileSync(`./part-i-the-big-picture/${fileName}`, {encoding:'utf8', flag:'r'}).replaceAll(/..\/(.*\.png)/ig, '../../../../../$1')
+    }
+  }).forEach(fileData => {
+    fs.writeFileSync(fileData.path, fileData.updatedContent, 'utf8');
+  });
+  const epubScript = malta.get().check(["the_better_way.json", ".gitbook", "-plugins=malta-epub"]).start();
 
-epubScript.then(() => {
+  epubScript.then(() => {
     epubScript.stop();
   });
+});
+
+
